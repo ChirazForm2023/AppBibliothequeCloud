6cd6a57673ec8dd9d4d73d15d1211876f7fa1af3
ChirazForm2023 <chiraz5edma@gmail.com>
2024-07-18 09:18:13 +0200

    first commit

---
+++ BackBibliotheque/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const sequelize = require("./config");
+const livreRoutes = require("./routes/livreRoutes");
+const auteurRoutes = require("./routes/auteurRoutes");
+
+const app = express();
+
+//Synchronisation de la BD
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database & tables created!");
+  })
+  .catch((err) => {
+    console.error("Unable to create tables:", err);
+  });
+
+app.use(express.json());
+app.use("/", livreRoutes);
+app.use("/", auteurRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log("Le serveur tourne sur le port ${PORT}");
+  });
+});
